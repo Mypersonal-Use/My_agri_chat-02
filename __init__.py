152697fea9d67b5002d9bf975f7399767596348c
Mypersonal-Use <prathuspren12@gmail.com>
2025-06-14 20:15:43 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,32 +2,45 @@
 
 from __future__ import annotations
 
-from .thread import Thread as Thread
-from .assistant import Assistant as Assistant
-from .function_tool import FunctionTool as FunctionTool
-from .assistant_tool import AssistantTool as AssistantTool
-from .thread_deleted import ThreadDeleted as ThreadDeleted
-from .file_search_tool import FileSearchTool as FileSearchTool
-from .assistant_deleted import AssistantDeleted as AssistantDeleted
-from .function_tool_param import FunctionToolParam as FunctionToolParam
-from .assistant_tool_param import AssistantToolParam as AssistantToolParam
-from .thread_create_params import ThreadCreateParams as ThreadCreateParams
-from .thread_update_params import ThreadUpdateParams as ThreadUpdateParams
-from .assistant_list_params import AssistantListParams as AssistantListParams
-from .assistant_tool_choice import AssistantToolChoice as AssistantToolChoice
-from .code_interpreter_tool import CodeInterpreterTool as CodeInterpreterTool
-from .assistant_stream_event import AssistantStreamEvent as AssistantStreamEvent
-from .file_search_tool_param import FileSearchToolParam as FileSearchToolParam
-from .assistant_create_params import AssistantCreateParams as AssistantCreateParams
-from .assistant_update_params import AssistantUpdateParams as AssistantUpdateParams
-from .assistant_tool_choice_param import AssistantToolChoiceParam as AssistantToolChoiceParam
-from .code_interpreter_tool_param import CodeInterpreterToolParam as CodeInterpreterToolParam
-from .assistant_tool_choice_option import AssistantToolChoiceOption as AssistantToolChoiceOption
-from .thread_create_and_run_params import ThreadCreateAndRunParams as ThreadCreateAndRunParams
-from .assistant_tool_choice_function import AssistantToolChoiceFunction as AssistantToolChoiceFunction
-from .assistant_response_format_option import AssistantResponseFormatOption as AssistantResponseFormatOption
-from .assistant_tool_choice_option_param import AssistantToolChoiceOptionParam as AssistantToolChoiceOptionParam
-from .assistant_tool_choice_function_param import AssistantToolChoiceFunctionParam as AssistantToolChoiceFunctionParam
-from .assistant_response_format_option_param import (
-    AssistantResponseFormatOptionParam as AssistantResponseFormatOptionParam,
-)
+from .run import Run as Run
+from .text import Text as Text
+from .message import Message as Message
+from .image_url import ImageURL as ImageURL
+from .annotation import Annotation as Annotation
+from .image_file import ImageFile as ImageFile
+from .run_status import RunStatus as RunStatus
+from .text_delta import TextDelta as TextDelta
+from .message_delta import MessageDelta as MessageDelta
+from .image_url_delta import ImageURLDelta as ImageURLDelta
+from .image_url_param import ImageURLParam as ImageURLParam
+from .message_content import MessageContent as MessageContent
+from .message_deleted import MessageDeleted as MessageDeleted
+from .run_list_params import RunListParams as RunListParams
+from .annotation_delta import AnnotationDelta as AnnotationDelta
+from .image_file_delta import ImageFileDelta as ImageFileDelta
+from .image_file_param import ImageFileParam as ImageFileParam
+from .text_delta_block import TextDeltaBlock as TextDeltaBlock
+from .run_create_params import RunCreateParams as RunCreateParams
+from .run_update_params import RunUpdateParams as RunUpdateParams
+from .text_content_block import TextContentBlock as TextContentBlock
+from .message_delta_event import MessageDeltaEvent as MessageDeltaEvent
+from .message_list_params import MessageListParams as MessageListParams
+from .refusal_delta_block import RefusalDeltaBlock as RefusalDeltaBlock
+from .file_path_annotation import FilePathAnnotation as FilePathAnnotation
+from .image_url_delta_block import ImageURLDeltaBlock as ImageURLDeltaBlock
+from .message_content_delta import MessageContentDelta as MessageContentDelta
+from .message_create_params import MessageCreateParams as MessageCreateParams
+from .message_update_params import MessageUpdateParams as MessageUpdateParams
+from .refusal_content_block import RefusalContentBlock as RefusalContentBlock
+from .image_file_delta_block import ImageFileDeltaBlock as ImageFileDeltaBlock
+from .image_url_content_block import ImageURLContentBlock as ImageURLContentBlock
+from .file_citation_annotation import FileCitationAnnotation as FileCitationAnnotation
+from .image_file_content_block import ImageFileContentBlock as ImageFileContentBlock
+from .text_content_block_param import TextContentBlockParam as TextContentBlockParam
+from .file_path_delta_annotation import FilePathDeltaAnnotation as FilePathDeltaAnnotation
+from .message_content_part_param import MessageContentPartParam as MessageContentPartParam
+from .image_url_content_block_param import ImageURLContentBlockParam as ImageURLContentBlockParam
+from .file_citation_delta_annotation import FileCitationDeltaAnnotation as FileCitationDeltaAnnotation
+from .image_file_content_block_param import ImageFileContentBlockParam as ImageFileContentBlockParam
+from .run_submit_tool_outputs_params import RunSubmitToolOutputsParams as RunSubmitToolOutputsParams
+from .required_action_function_tool_call import RequiredActionFunctionToolCall as RequiredActionFunctionToolCall
