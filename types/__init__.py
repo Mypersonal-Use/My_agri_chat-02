5942e41375ada1525243ad4f7fb7813cb9e8843c
Mypersonal-Use <prathuspren12@gmail.com>
2025-06-15 08:41:34 +0530

    Add files via upload

--- types/__init__.py
+++ types/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2023 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,124 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""A collection of type definitions used throughout the library."""
+#
+from .citation import CitationMetadata, CitationSource
+from .discuss_service import (
+    CountMessageTokensRequest,
+    CountMessageTokensResponse,
+    Example,
+    GenerateMessageRequest,
+    GenerateMessageResponse,
+    Message,
+    MessagePrompt,
+)
+from .model import Model
+from .model_service import (
+    CreateTunedModelMetadata,
+    CreateTunedModelRequest,
+    DeleteTunedModelRequest,
+    GetModelRequest,
+    GetTunedModelRequest,
+    ListModelsRequest,
+    ListModelsResponse,
+    ListTunedModelsRequest,
+    ListTunedModelsResponse,
+    UpdateTunedModelRequest,
+)
+from .permission import Permission
+from .permission_service import (
+    CreatePermissionRequest,
+    DeletePermissionRequest,
+    GetPermissionRequest,
+    ListPermissionsRequest,
+    ListPermissionsResponse,
+    TransferOwnershipRequest,
+    TransferOwnershipResponse,
+    UpdatePermissionRequest,
+)
+from .safety import (
+    ContentFilter,
+    HarmCategory,
+    SafetyFeedback,
+    SafetyRating,
+    SafetySetting,
+)
+from .text_service import (
+    BatchEmbedTextRequest,
+    BatchEmbedTextResponse,
+    CountTextTokensRequest,
+    CountTextTokensResponse,
+    Embedding,
+    EmbedTextRequest,
+    EmbedTextResponse,
+    GenerateTextRequest,
+    GenerateTextResponse,
+    TextCompletion,
+    TextPrompt,
+)
+from .tuned_model import (
+    Dataset,
+    Hyperparameters,
+    TunedModel,
+    TunedModelSource,
+    TuningExample,
+    TuningExamples,
+    TuningSnapshot,
+    TuningTask,
+)
 
-from google.generativeai.types.citation_types import *
-from google.generativeai.types.content_types import *
-from google.generativeai.types.file_types import *
-from google.generativeai.types.generation_types import *
-from google.generativeai.types.helper_types import *
-from google.generativeai.types.model_types import *
-from google.generativeai.types.permission_types import *
-from google.generativeai.types.safety_types import *
-
-
-del model_types
-del citation_types
-del safety_types
+__all__ = (
+    "CitationMetadata",
+    "CitationSource",
+    "CountMessageTokensRequest",
+    "CountMessageTokensResponse",
+    "Example",
+    "GenerateMessageRequest",
+    "GenerateMessageResponse",
+    "Message",
+    "MessagePrompt",
+    "Model",
+    "CreateTunedModelMetadata",
+    "CreateTunedModelRequest",
+    "DeleteTunedModelRequest",
+    "GetModelRequest",
+    "GetTunedModelRequest",
+    "ListModelsRequest",
+    "ListModelsResponse",
+    "ListTunedModelsRequest",
+    "ListTunedModelsResponse",
+    "UpdateTunedModelRequest",
+    "Permission",
+    "CreatePermissionRequest",
+    "DeletePermissionRequest",
+    "GetPermissionRequest",
+    "ListPermissionsRequest",
+    "ListPermissionsResponse",
+    "TransferOwnershipRequest",
+    "TransferOwnershipResponse",
+    "UpdatePermissionRequest",
+    "ContentFilter",
+    "SafetyFeedback",
+    "SafetyRating",
+    "SafetySetting",
+    "HarmCategory",
+    "BatchEmbedTextRequest",
+    "BatchEmbedTextResponse",
+    "CountTextTokensRequest",
+    "CountTextTokensResponse",
+    "Embedding",
+    "EmbedTextRequest",
+    "EmbedTextResponse",
+    "GenerateTextRequest",
+    "GenerateTextResponse",
+    "TextCompletion",
+    "TextPrompt",
+    "Dataset",
+    "Hyperparameters",
+    "TunedModel",
+    "TunedModelSource",
+    "TuningExample",
+    "TuningExamples",
+    "TuningSnapshot",
+    "TuningTask",
+)
